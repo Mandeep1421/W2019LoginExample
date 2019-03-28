141c8ef5c143cd33561cceaac020dee9c054585c
Mandeep1421 <Mandeep1421@users.noreply.github.com>
2019-03-28 19:47:06 -0400

    Login coding is added

--- app/src/main/java/com/mandy/w2019loginexample/MainActivity.java
+++ app/src/main/java/com/mandy/w2019loginexample/MainActivity.java
@@ -2,12 +2,57 @@ package com.mandy.w2019loginexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private EditText editEmail;
+    private EditText editPassword;
+    private Button BtnLogin;
+    private Button Btnsignup;
+    private TextView txtMsg;
+    String Pass;
+    String email;
+    String lblError;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editEmail = findViewById(R.id.editEmail);
+        editPassword = findViewById(R.id.editPassword);
+        txtMsg = findViewById(R.id.txtMsg);
+
+        BtnLogin = findViewById(R.id.BtnLogin);
+        Btnsignup = findViewById(R.id.btnsignup);
+
+        BtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                email = editEmail.getText().toString();
+                Pass = editPassword.getText().toString();
+
+                if(email.equals("Admin")  && Pass.equals("123"))
+                {
+                    lblError = "Login SuccessFull";
+
+                }
+                else
+                {
+                    lblError = "Invalid Username or Password!";
+                }
+                txtMsg.setText(lblError.toString());
+
+                String email = editEmail.getText().toString();
+                Toast.makeText(getApplicationContext(), email, Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
